e39e44b2cef83988ea1a0612dfb92138d08b30dc
xue0211 <zhengtai1123.qq.com>
2025-01-29 19:09:06 +0800

    初始化项目

---
+++ gulu-ui-1/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>hi</div>
+ <Xue/>
+</template>
+
+<script>
+import Xue from './components/Xue.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Xue:Xue  // 标签名：组件的引用
+  }
+}
+</script>
---
+++ gulu-ui-1/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue' // 创建APP实例的函数
+import App from './App.vue'
+import './index.css'
+
+createApp(App).mount('#app')
